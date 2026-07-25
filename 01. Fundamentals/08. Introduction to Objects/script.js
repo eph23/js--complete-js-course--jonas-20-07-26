5e26160733779cd039adaf35d4a14f1f6482b21d
Eph23 <ephraims.818@gmail.com>
2026-07-26 01:45:13 +0600

    JS--COMPLETE-JS--REV -- FUNDAMENTALS -- 08. Introduction to Objects

---
+++ 01. Fundamentals/08. Introduction to Objects/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// ANCHOR: Object literal
+/* NOTE: In JavaScript, an object is a standalone entity that stores data in key-value pairs, allowing you to group related variables (properties) and functions (methods) together.
+ */
+
+const eph = {
+    firstName: "Ephraim",
+    lastName: "S",
+    age: 2037 - 1988,
+    job: "Web Dev",
+    friends: ["Michael", "Steven", "Bob"],
+};
+console.log(eph);
