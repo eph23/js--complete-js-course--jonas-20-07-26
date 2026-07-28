b260c0006c419c917645ffb85e5e18317025f0e2
Eph23 <ephraims.818@gmail.com>
2026-07-28 22:16:41 +0600

    JS--COMPLETE-JS--REV -- FUNDAMENTALS -- 11. Iteration The for Loop

---
+++ 01. Fundamentals/11. Iteration The for Loop/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// ANCHOR: The for loop
+/* NOTE: 
+A JavaScript for loop repeats a block of code a specific number of times based on three optional expressions enclosed in parentheses. It is an entry-controlled loop, meaning it checks the condition before executing any code inside its block.
+*/
+
+for (let rep = 1; rep <= 5; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
