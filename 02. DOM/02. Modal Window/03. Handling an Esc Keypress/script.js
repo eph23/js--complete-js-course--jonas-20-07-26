231456bf361224fd69ee3c4c1b718f61b9b5af0c
Eph23 <ephraims.818@gmail.com>
2026-08-01 02:49:34 +0600

    JS--COMPLETE-JS--REV -- DOM -- MODAL WINDOW - 03. Handling an Esc Keypress

---
+++ 02. DOM/02. Modal Window/03. Handling an Esc Keypress/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const openModalBtn = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+
+const closeModel = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+openModalBtn.forEach(btn =>
+  btn.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  }),
+);
+
+closeModalBtn.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModel();
+  }
+});
