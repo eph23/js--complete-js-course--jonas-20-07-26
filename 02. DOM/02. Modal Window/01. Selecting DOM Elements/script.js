cea32a844ac25ddaf9acc470da5b1ceea10875ae
Eph23 <ephraims.818@gmail.com>
2026-08-01 02:37:28 +0600

    JS--COMPLETE-JS--REV -- DOM -- MODAL WINDOW - 01. Selecting DOM Elements

---
+++ 02. DOM/02. Modal Window/01. Selecting DOM Elements/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modal = document.querySelectorAll('.modal');
+const overlay = document.querySelectorAll('.overlay');
+const openModalBtn = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelectorAll('.close-modal');
