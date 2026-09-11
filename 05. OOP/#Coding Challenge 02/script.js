7256814a5e9dbc68355d3e7b51ed6289366804a1
Eph23 <ephraims.818@gmail.com>
2026-09-12 03:18:49 +0600

    JS--COMPLETE-JS--REV -- OOP -- CODING CHALLENGE #02

---
+++ 05. OOP/#Coding Challenge 02/script.js
@@ -0,0 +1,73 @@
+'use strict';
+
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+/* ============================================ */
+/*                   SOLUTION                   */
+// Objective 1
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(
+      `Woohooo.... Your ${this.make}'s current speed is ${this.speed} km/h`,
+    );
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(
+      `BUT WHY!!! Your ${this.make}'s current speed is ${this.speed} km/h`,
+    );
+  }
+
+  get speedUS() {
+    return `Current speed in miles per hour is ${this.speed / 1.6} mi/h`;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+// Objective 4
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
+
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+console.log(ford.speedUS);
+
+ford.speedUS = 50;
+console.log(ford.speedUS);
+console.log(ford);
+/* ============================================ */
