00c209252d40882f629c1f24a2aec27cdea922da
Eph23 <ephraims.818@gmail.com>
2026-09-12 03:00:09 +0600

    JS--COMPLETE-JS--REV -- OOP -- 07. Object.create

---
+++ 05. OOP/07. Object.create/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+console.log(sarah)
