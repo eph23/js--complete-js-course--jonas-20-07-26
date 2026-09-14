aed198d1cad11b6b1700203d48a2e8ce5c76d4b0
Eph23 <ephraims.818@gmail.com>
2026-09-14 17:46:45 +0600

    JS--COMPLETE-JS--REV -- OOP -- 11. Another Class Example

---
+++ 05. OOP/11. Another Class Example/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${this.owner}`);
+  }
+
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    console.log(`Your loan has been approved`);
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val))
+      console.log(`Your loan request of $${val} is approved`);
+  }
+}
+
+const acc1 = new Account('Eph', 'EUR', 1111);
+console.log(acc1);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
