5211c0be82fe275e6ee8702b2119fb7cc862a4de
Eph23 <ephraims.818@gmail.com>
2026-09-17 19:08:05 +0600

    JS--COMPLETE-JS--REV -- Async JS -- 03. Consuming Promises

---
+++ 06. Async/03. Consuming Promises/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data, className = '') {
+  const html = `
+          <article class="country ${className}">
+               <img class="country__img" src=${data.flags.png} />
+               <div class="country__data">
+               <h3 class="country__name">${data.name}</h3>
+               <h4 class="country__region">${data.region}</h4>
+               <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(2)} M people</p>
+               <p class="country__row"><span>🗣️</span>${data.languages
+                 .map(lang => lang.name)
+                 .join(', ')}</p>
+               <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+               </div>
+          </article>
+    `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  const request = fetch(`https://countries.dev/name/${country}`)
+    .then(response => response.json()).then(data => renderCountry(data[0])
+    )
+};
+
+getCountryData('bangladesh');
+getCountryData('canada');
